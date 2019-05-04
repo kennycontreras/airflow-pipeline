1db962bf401d61163f3add981f21c7d74a9fea98
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-05-04 12:48:21 -0400

    Add delete statement for dimension operator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -12,20 +12,36 @@ class LoadDimensionOperator(BaseOperator):
                  postgres_conn_id='',
                  table='',
                  sql_query='',
+                 load_mode='',
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.postgres_conn_id = postgres_conn_id
         self.table = table
         self.sql_query = sql_query
+        self.load_mode = load_mode
 
     def execute(self, context):
+
         redshift = PostgresHook(self.postgres_conn_id)
-        self.log.info('Loading fact table')
+        # truncate statement
+        truncate_sql = """
+                    TRUNCATE {};
+                """.format(self.table)
 
+        # insert statement
         insert_sql = """
-            INSERT INTO {}
-            {}
-        """.format(self.table, self.sql_query)
+                    INSERT INTO {}
+                    {};
+                """.format(self.table, self.sql_query)
 
-        redshift.run(insert_sql)
+        if self.load_mode == 'truncate':
+
+            self.log.info('Truncating and loading dimesion table')
+            # Run truncate first and then insert statement
+            redshift.run(truncate_sql + '\n' + insert_sql)
+
+        else:
+            self.log.info('Loading dimesion table')
+            # Run insert statement
+            redshift.run(insert_sql)
